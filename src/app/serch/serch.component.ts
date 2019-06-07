208480db78e2c9779bcddd08b885e5d9fff53fcc
johnwanjema <jonwanjema@gmail.com>
2019-06-07 12:13:34 +0300

    added button for search

--- src/app/serch/serch.component.ts
+++ src/app/serch/serch.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SerchComponent implements OnInit {
 
+  fun(){
+    alert('qwertyui');
+  }
+
   constructor() { }
+
 
   ngOnInit() {
   }
