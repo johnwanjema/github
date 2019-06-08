03cedb0b8a4e44a87bf9217e3a1dd3cfc60c6f14
johnwanjema <jonwanjema@gmail.com>
2019-06-08 20:59:35 +0300

    addedroute modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { UserService } from './services/user.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 
