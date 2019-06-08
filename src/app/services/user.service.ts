df97a8f03e7b1b427b068ee980ae2376826d1807
johnwanjema <jonwanjema@gmail.com>
2019-06-08 08:49:30 +0300

    added providers

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'
+import { HttpClient } from '@angular/common/http';
+import { getDefaultService } from 'selenium-webdriver/chrome';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private username: string;
+  private clientid: 'Iv1.bf178c2636e78d47';
+  private clientsecret: 'c40e5c5b7748f0e358584878c86db85edbec72ee';
+  constructor(private http: HttpClient) {
+    this.username = 'johnwanjema'
+
+  }
+  getuser() {
+    return this.http.get('https://api.github.com/users/' + this.username + "?client_id =" + this.clientid + "&client_secret=" + this.clientsecret)
+    .map(res => res.json());
+  }
+
 }
