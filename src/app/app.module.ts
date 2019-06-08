51fdd1fba551a1316b843a2596a02d77ad5498af
johnwanjema <jonwanjema@gmail.com>
2019-06-08 21:23:31 +0300

    added routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { RepoService } from './services/repo.service';
-import {SearchrepoService} from './services/searchrepo.service'
+import { SearchrepoService } from './services/searchrepo.service'
 import { UserService } from './services/user.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+// Defining routes
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'searchuser', component: searchComponent }
+]
 
 
-
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { searchComponent } from './search/search.component';
 import { HttpModule } from '@angular/http';
@@ -24,10 +28,11 @@ import { ProfileComponent } from './components/profile/profile.component';
     BrowserModule,
     HttpModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [UserService,
-    RepoService,SearchrepoService
+    RepoService, SearchrepoService
   ],
   bootstrap: [AppComponent]
 })
